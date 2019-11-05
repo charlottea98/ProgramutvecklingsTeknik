af87f3fc97a39c5d969748f13e33ec59e9eda525
Charlotte Andersson <charlotteandersson6@gmail.com>
2019-11-05 10:41:51 +0100

    lab2

---
+++ Lab2/ViewControl.java
@@ -0,0 +1,15 @@
+package Lab2;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class ViewControl extends JFrame implements ActionListener {
+    private Boardgame game;
+    private int size;
+    private Square[][] board;
+    private JLabel mess = new JLabel();
+    
+    ViewControl(Boardgame gm, int n) {
+
+    }
+}
